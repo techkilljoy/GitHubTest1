4bf6d104482a229ff7ade5f3bdd3e2bf1f935e34
techkilljoy <techkilljoy@gmail.com>
2014-09-26 09:47:10 -0600

    Initial program files

---
+++ GitHubTest1/Form1.Designer.vb
@@ -0,0 +1,63 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.btnGreeting = New System.Windows.Forms.Button()
+        Me.lblGreeting = New System.Windows.Forms.Label()
+        Me.SuspendLayout()
+        '
+        'btnGreeting
+        '
+        Me.btnGreeting.Location = New System.Drawing.Point(106, 64)
+        Me.btnGreeting.Name = "btnGreeting"
+        Me.btnGreeting.Size = New System.Drawing.Size(75, 23)
+        Me.btnGreeting.TabIndex = 0
+        Me.btnGreeting.Text = "Greeting"
+        Me.btnGreeting.UseVisualStyleBackColor = True
+        '
+        'lblGreeting
+        '
+        Me.lblGreeting.AutoSize = True
+        Me.lblGreeting.Location = New System.Drawing.Point(118, 142)
+        Me.lblGreeting.Name = "lblGreeting"
+        Me.lblGreeting.Size = New System.Drawing.Size(39, 13)
+        Me.lblGreeting.TabIndex = 1
+        Me.lblGreeting.Text = "Label1"
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(284, 262)
+        Me.Controls.Add(Me.lblGreeting)
+        Me.Controls.Add(Me.btnGreeting)
+        Me.Name = "Form1"
+        Me.Text = "Form1"
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+    Friend WithEvents btnGreeting As System.Windows.Forms.Button
+    Friend WithEvents lblGreeting As System.Windows.Forms.Label
+
+End Class
---
+++ GitHubTest1/Form1.vb
@@ -0,0 +1,6 @@
+﻿Public Class Form1
+
+    Private Sub btnGreeting_Click(sender As Object, e As EventArgs) Handles btnGreeting.Click
+        Me.lblGreeting.Text = "Hello GitHub!"
+    End Sub
+End Class
